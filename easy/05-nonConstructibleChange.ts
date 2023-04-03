3fedbe1c7623252f4fdd7375139231c1b668807b
Jean Paul <yjeanpaul@gmail.com>
2023-04-03 13:35:50 -0500

    Solved 2 problems of algoExpert

---
+++ easy/05-nonConstructibleChange.ts
@@ -0,0 +1,19 @@
+
+export function nonConstructibleChange(coins: number[]) {
+  // Write your code here.
+  const sortedCoins = coins.sort((number1, number2) => number1 - number2);
+  let maximumChange = 0;
+
+  for (const coinValue of sortedCoins) {
+    if (coinValue <= maximumChange + 1) {
+      maximumChange += coinValue;
+    }else {
+      break;
+    }
+
+  }
+
+  return maximumChange + 1;
+}
+
+console.log(nonConstructibleChange([5, 7, 1, 1, 2, 3, 22]));
---
+++ easy/06-findClosestValueInBst.ts
@@ -0,0 +1,38 @@
+class BST {
+  value: number;
+  left: BST | null;
+  right: BST | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export function findClosestValueInBst(tree: BST, target: number) {
+  if (!tree.value) {
+    return -1;
+  }
+  let node: BST | null = tree;
+  let answer = {
+    value: tree.value,
+    distance: Math.abs(tree.value - target),
+  };
+  while (node !== null) {
+    const distance = Math.abs(node.value - target);
+    if (distance < answer.distance ) {
+        answer = {
+            value: node.value,
+            distance,
+        }
+    }
+    if (target > node.value) {
+        node = node.right;
+    } else {
+        node = node.left;
+    }
+  }
+
+  return answer.value;
+}
